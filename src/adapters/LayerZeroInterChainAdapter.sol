82e3e9772ec9c5033f983817d6ad265f4aead6a7
iammrjude <judeabara@gmail.com>
2023-12-17 17:26:50 +0100

    prepare bridge to add interchain adapter

---
+++ src/adapters/LayerZeroInterChainAdapter.sol
@@ -0,0 +1,80 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "../core/base/Controllable.sol";
+import "../interfaces/IInterChainAdapter.sol";
+import "../interfaces/IChildTokenFactory.sol";
+import "./lzApp/NonblockingLzApp.sol";
+
+/// @author Jude (https://github.com/iammrjude)
+contract LayerZeroInterChainAdapter is Controllable, IInterChainAdapter, NonblockingLzApp {
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                         CONSTANTS                          */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    bytes public constant PAYLOAD = "\x01\x02\x03\x04";
+
+    /// @dev Version of LayerZeroInterChainAdapter implementation
+    string public constant VERSION = "1.0.0";
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       INITIALIZATION                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function initialize(address platform_, address _lzEndpoint) external initializer {
+        __Controllable_init(platform_);
+        __NonblockingLzApp_init(_lzEndpoint);
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                      RESTRICTED ACTIONS                    */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @inheritdoc IInterChainAdapter
+    function sendMessage(Message memory message) external {}
+
+    /// @inheritdoc IInterChainAdapter
+    function interChainAdapterId() external returns (string memory) {}
+
+    function setOracle(uint16 dstChainId, address oracle) external onlyOwner {
+        uint TYPE_ORACLE = 6;
+        // set the Oracle
+        lzEndpoint.setConfig(lzEndpoint.getSendVersion(address(this)), dstChainId, TYPE_ORACLE, abi.encode(oracle));
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       VIEW FUNCTIONS                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    /// @inheritdoc IInterChainAdapter
+    function endpoint() external view returns (address) {}
+
+    function estimateFee(
+        uint16 _dstChainId,
+        bool _useZro,
+        bytes calldata _adapterParams
+    ) public view returns (uint nativeFee, uint zroFee) {
+        return lzEndpoint.estimateFees(_dstChainId, address(this), PAYLOAD, _useZro, _adapterParams);
+    }
+
+    function getOracle(uint16 remoteChainId) external view returns (address _oracle) {
+        bytes memory bytesOracle =
+            lzEndpoint.getConfig(lzEndpoint.getSendVersion(address(this)), remoteChainId, address(this), 6);
+        assembly {
+            _oracle := mload(add(bytesOracle, 32))
+        }
+    }
+
+    /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
+    /*                       INTERNAL LOGIC                       */
+    /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
+
+    function _nonblockingLzReceive(uint16, bytes memory, uint64, bytes memory _payload) internal override {
+        (address toAddress, uint amountOrTokenId, address token, bool nft, bool mint) =
+            abi.decode(_payload, (address, uint, address, bool, bool));
+        // address childToken = IChildTokenFactory(_getStorage().childTokenFactory).getChildTokenOf(token);
+        // address parentToken = IChildTokenFactory(_getStorage().childTokenFactory).getParentTokenOf(token);
+        // if (mint) mintToken(childToken, toAddress, amountOrTokenId, nft);
+        // if (!mint) unlockToken(parentToken, toAddress, amountOrTokenId, nft);
+    }
+}
--- src/bridge/Bridge.sol
+++ src/bridge/Bridge.sol
@@ -8,17 +8,14 @@ import "../core/base/Controllable.sol";
 import "../interfaces/IBridge.sol";
 import "../interfaces/IChildERC20.sol";
 import "../interfaces/IChildERC721.sol";
-import "../interfaces/IChildTokenFactory.sol";
-import "./lzApp/NonblockingLzApp.sol";
+import "../interfaces/IInterChainAdapter.sol";
 
 /// @title Stability Bridge
 /// @author Jude (https://github.com/iammrjude)
-contract Bridge is Controllable, IBridge, NonblockingLzApp {
+contract Bridge is Controllable, IBridge {
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                         CONSTANTS                          */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
-
-    bytes public constant PAYLOAD = "\x01\x02\x03\x04";
 
     /// @inheritdoc IControllable
     string public constant VERSION = "1.0.0";
@@ -42,9 +39,8 @@ contract Bridge is Controllable, IBridge, NonblockingLzApp {
     /*                       INITIALIZATION                       */
     /*.•°:°.´+˚.*°.˚:*.´•*.+°.•°:´*.´•*.•°.•°:°.´:•˚°.*°.˚:*.´+°.•*/
 
-    function initialize(address platform_, address _lzEndpoint) external initializer {
+    function initialize(address platform_) external initializer {
         __Controllable_init(platform_);
-        __NonblockingLzApp_init(_lzEndpoint);
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -74,22 +70,6 @@ contract Bridge is Controllable, IBridge, NonblockingLzApp {
 
     /// @inheritdoc IBridge
     function getTarget(address token, uint64 chainTo) external view returns (address targetToken, bytes32 linkHash) {}
-
-    function estimateFee(
-        uint16 _dstChainId,
-        bool _useZro,
-        bytes calldata _adapterParams
-    ) public view returns (uint nativeFee, uint zroFee) {
-        return lzEndpoint.estimateFees(_dstChainId, address(this), PAYLOAD, _useZro, _adapterParams);
-    }
-
-    function getOracle(uint16 remoteChainId) external view returns (address _oracle) {
-        bytes memory bytesOracle =
-            lzEndpoint.getConfig(lzEndpoint.getSendVersion(address(this)), remoteChainId, address(this), 6);
-        assembly {
-            _oracle := mload(add(bytesOracle, 32))
-        }
-    }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
     /*                      RESTRICTED ACTIONS                    */
@@ -135,7 +115,7 @@ contract Bridge is Controllable, IBridge, NonblockingLzApp {
             if (!success) revert TokenTransferFailed();
         }
         bytes memory payload = abi.encode(msg.sender, amountOrTokenId, token, nft, true);
-        _lzSend(chainTo, payload, payable(msg.sender), address(0x0), bytes(""), msg.value);
+        // _lzSend(chainTo, payload, payable(msg.sender), address(0x0), bytes(""), msg.value);
     }
 
     function burnToken(address token, uint amountOrTokenId, uint16 chainTo, bool nft) public payable {
@@ -145,13 +125,7 @@ contract Bridge is Controllable, IBridge, NonblockingLzApp {
             IChildERC20(token).burn(msg.sender, amountOrTokenId);
         }
         bytes memory payload = abi.encode(msg.sender, amountOrTokenId, token, nft, false);
-        _lzSend(chainTo, payload, payable(msg.sender), address(0x0), bytes(""), msg.value);
-    }
-
-    function setOracle(uint16 dstChainId, address oracle) external onlyOwner {
-        uint TYPE_ORACLE = 6;
-        // set the Oracle
-        lzEndpoint.setConfig(lzEndpoint.getSendVersion(address(this)), dstChainId, TYPE_ORACLE, abi.encode(oracle));
+        // _lzSend(chainTo, payload, payable(msg.sender), address(0x0), bytes(""), msg.value);
     }
 
     /*´:°•.°+.*•´.*:˚.°*.˚•´.°:°•.°•.*•´.*:˚.°*.˚•´.°:°•.°+.*•´.*:*/
@@ -180,14 +154,5 @@ contract Bridge is Controllable, IBridge, NonblockingLzApp {
             bool success = IERC20(token).transfer(to, amountOrTokenId);
             if (!success) revert TokenTransferFailed();
         }
-    }
-
-    function _nonblockingLzReceive(uint16, bytes memory, uint64, bytes memory _payload) internal override {
-        (address toAddress, uint amountOrTokenId, address token, bool nft, bool mint) =
-            abi.decode(_payload, (address, uint, address, bool, bool));
-        address childToken = IChildTokenFactory(_getStorage().childTokenFactory).getChildTokenOf(token);
-        address parentToken = IChildTokenFactory(_getStorage().childTokenFactory).getParentTokenOf(token);
-        if (mint) mintToken(childToken, toAddress, amountOrTokenId, nft);
-        if (!mint) unlockToken(parentToken, toAddress, amountOrTokenId, nft);
     }
 }
