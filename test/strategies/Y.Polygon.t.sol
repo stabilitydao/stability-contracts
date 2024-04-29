0df40aeb32f83cd1bb7c580f73e99ba6adc56140
Alien Deployer <m@a17.name>
2024-04-29 17:32:24 +0300

    lint

--- test/strategies/Y.Polygon.t.sol
+++ test/strategies/Y.Polygon.t.sol
@@ -6,21 +6,16 @@ import "../base/UniversalTest.sol";
 
 contract YearnStrategyTest is PolygonSetup, UniversalTest {
     function testYearnStrategy() public universalTest {
-         _addStrategy(PolygonLib.YEARN_USDCe);
-         _addStrategy(PolygonLib.YEARN_DAI);
-         _addStrategy(PolygonLib.YEARN_USDT);
-         _addStrategy(PolygonLib.YEARN_WMATIC);
-         _addStrategy(PolygonLib.YEARN_WETH);
+        _addStrategy(PolygonLib.YEARN_USDCe);
+        _addStrategy(PolygonLib.YEARN_DAI);
+        _addStrategy(PolygonLib.YEARN_USDT);
+        _addStrategy(PolygonLib.YEARN_WMATIC);
+        _addStrategy(PolygonLib.YEARN_WETH);
     }
 
     function _addStrategy(address yaernV3Vault) internal {
         strategies.push(
-            Strategy({
-                id: StrategyIdLib.YEARN,
-                pool: address(0),
-                farmId: type(uint).max,
-                underlying: yaernV3Vault
-            })
+            Strategy({id: StrategyIdLib.YEARN, pool: address(0), farmId: type(uint).max, underlying: yaernV3Vault})
         );
     }
 }
