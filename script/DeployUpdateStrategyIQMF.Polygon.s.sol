b677a7598bd9a5d1fce227b50a10e5a03bca33e6
hcrypto7 <levilee9571@gmail.com>
2024-05-16 09:52:10 -0500

    added deploy script

---
+++ script/DeployUpdateStrategyIQMF.Polygon.s.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+import "../src/strategies/IchiQuickSwapMerklFarmStrategy.sol";
+
+contract DeployUpdateStrategyIQMFPolygon is Script {
+    function run() external {
+        uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        new IchiQuickSwapMerklFarmStrategy();
+
+        vm.stopBroadcast();
+    }
+
+    function testDeployPolygon() external {}
+}
