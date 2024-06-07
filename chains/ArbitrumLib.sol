0e99cf15aa1a1cfba2711fafca813f4a3f7c4e57
interlinker0115 <interlinker422031@gmail.com>
2024-06-08 07:13:23 +1000

    create arbitrum lib

---
+++ chains/ArbitrumLib.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+/// @dev Arbitrum network [chainId: 42161] data library
+///      ┳┓
+///      ┣┫┏┓┏┏┓
+///      ┻┛┗┻┛┗
+/// @author Alien Deployer (https://github.com/a17)
+library ArbitrumLib {
+    // initial addresses
+
+    // ERC20
+
+    // AMMs
+
+    // Oracles
+
+    // Compound
+
+    // DeX aggregators
+
+}
