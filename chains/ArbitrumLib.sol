f32fe4b21bf1491a87eed13f8f7e3639e26b6314
interlinker0115 <interlinker422031@gmail.com>
2024-06-10 04:45:30 +1000

    add: deploy and setup oracle adapters

--- chains/ArbitrumLib.sol
+++ chains/ArbitrumLib.sol
@@ -2,6 +2,12 @@
 pragma solidity ^0.8.23;
 
 import "../src/interfaces/IPlatformDeployer.sol";
+import "../src/interfaces/IFactory.sol";
+import "../src/interfaces/IPlatform.sol";
+import "../src/core/proxy/Proxy.sol";
+import "../src/adapters/ChainlinkAdapter.sol";
+import "../script/libs/LogDeployLib.sol";
+
 
 /// @dev Arbitrum network [chainId: 42161] data library
 
@@ -19,10 +25,15 @@ library ArbitrumLib {
     // ERC20
     address public constant TOKEN_ARB = 0x912CE59144191C1204E64559FE8253a0e49E6548;
     address public constant TOKEN_USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address public constant TOKEN_USDT = 0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9;
+    address public constant TOKEN_DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
     
     // AMMs
 
     // Oracles
+    address public constant ORACLE_CHAINLINK_USDC_USD = 0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3;
+    address public constant ORACLE_CHAINLINK_USDT_USD = 0x3f3f5dF88dC9F13eac63DF89EC16ef6e7E25DdE7;
+    address public constant ORACLE_CHAINLINK_DAI_USD = 0xc5C8E77B397E531B8EC06BFb0048328B30E9eCfB;
 
     // Compound
 
@@ -41,6 +52,42 @@ library ArbitrumLib {
         p.gelatoDepositAmount = 2e16;
     }
 
+    function deployAndSetupInfrastructure(address platform, bool showLog) internal {
+        IFactory factory = IFactory(IPlatform(platform).factory());
+        
+        //region ----- Deployed Platform -----
+        if(showLog) {
+            console.log("Deployed Statbility platform", IPlatform(platform).platformVersion());
+            console.log("Platform addres:", platform);
+        }
+        //endregion -- Deployed Platform ----
 
+        //region ----- Deploy and setup vault types -----
+        _addVaultType(factory, VaultTypeLib.COMPOUNDING, address(new CVault()), 100e6);
+        //endregion -- Deploy and setup valut types -----
 
+        //region -----Deploy and setup oracle adapters -----
+        IPriceReader priceReader = PriceReader(IPlatform(platform).priceReader());
+        //Chainlnk
+        {
+            Proxy proxy = new Proxy();
+            proxy.initProxy(address(new ChainlinkAdapter()));
+            ChainlinkAdapter chainlinkAdapter = ChainlinkAdapter(address(proxy));
+            chainlinkAdapter.initialize(platform);
+            address[] memory assets = new address[](3);
+            assets[0] = TOKEN_USDC;
+            assets[1] = TOKEN_USDT;
+            assets[2] = TOKEN_DAI;
+            address[] memory priceFeeds = new address[](3);
+            priceFeeds[0] = ORACLE_CHAINLINK_USDC_USD;
+            priceFeeds[1] = ORACLE_CHAINLINK_USDT_USD;
+            priceFeeds[2] = ORACLE_CHAINLINK_DAI_USD;
+            chainlinkAdapter.addPriceFeeds(assets, priceFeeds);
+            priceReader.addAdapter(address(chainlinkAdapter));
+            LogDeployLib.logDeployAndSetupOracleAdapter("ChainLink", address(chainlinkAdapter), showLog);
+        }
+        //endregion -- Deploy and setup oracle adapters -----
+        
+
+    }
 }
