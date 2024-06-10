f0dd6ac0e3a77b219dbaf55d4bf1aee39ef2a4f5
interlinker0115 <interlinker422031@gmail.com>
2024-06-10 17:57:27 +1000

    add: setup swapper and deploy strategy logics

--- chains/ArbitrumLib.sol
+++ chains/ArbitrumLib.sol
@@ -9,15 +9,16 @@ import "../src/adapters/ChainlinkAdapter.sol";
 import "../script/libs/LogDeployLib.sol";
 import "../script/libs/DeployAdapterLib.sol";
 import "../src/adapters/libs/AmmAdapterIdLib.sol";
-
+import "../src/interfaces/ISwapper.sol";
+import "../src/strategies/libs/StrategyIdLib.sol";
 
 /// @dev Arbitrum network [chainId: 42161] data library
 
-//   AAAAA  RRRR   BBBB    III  TTTTTT UU   UU MMMM   MMMM
-//  AA   AA RR  RR BB  BB  III    TT   UU   UU MM MM MM MM
-//  AA   AA RRRR   BBBBB   III    TT   UU   UU MM  MMM  MM
-//  AAAAAAA RR  RR BB  BB  III    TT   UU   UU MM       MM
-//  AA   AA RR   RR BBBB   III    TT    UUUUU  MM       MM
+//   AAAAA  RRRR   BBBB    III TTTTTT RRRR   UU   UU MMMM   MMMM
+//  AA   AA RR  RR BB  BB  III   TT   RR  RR UU   UU MM MM MM MM
+//  AA   AA RRRR   BBBBB   III   TT   RRRR   UU   UU MM  MMM  MM
+//  AAAAAAA RR  RR BB  BB  III   TT   RR  RR UU   UU MM       MM
+//  AA   AA RR   RR BBBB   III   TT   RR   RR UUUUU  MM       MM
 
 /// @author Alien Deployer (https://github.com/a17)
 library ArbitrumLib {
@@ -29,6 +30,11 @@ library ArbitrumLib {
     address public constant TOKEN_USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
     address public constant TOKEN_USDT = 0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9;
     address public constant TOKEN_DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
+    address public constant TOKEN_USDCe = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address public constant TOKEN_WBTC = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
+    address public constant TOKEN_weETH = 0x35751007a407ca6FEFfE80b3cB397736D2cf4dbe;
+    address public constant TOKEN_frxETH = 0x178412e79c25968a32e89b11f63B33F733770c2A;
+    address public constant TOKEN_FRAX = 0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F;
     
     // AMMs
 
@@ -96,5 +102,70 @@ library ArbitrumLib {
         LogDeployLib.deployAmmAdapter(platform, showLog);
         //endregion -- Deploy AMM adapters -----
 
+        //region ----- Setup Swapper -----
+        {
+            (ISwapper.AddPoolData[] memory bcPools, ISwapper.AddPoolData[] memory pools) = routes();
+            ISwapper swapper = ISwapper(IPlatform(platform).swapper());
+            swapper.addBlueChipsPools(bcPools, false);
+            swapper.addPools(pools, false);
+            address[] memory tokenIn = new address[](8);
+            tokenIn[0] = TOKEN_USDC;
+            tokenIn[1] = TOKEN_USDT;
+            tokenIn[2] = TOKEN_DAI;
+            tokenIn[3] = TOKEN_USDCe;
+            tokenIn[4] = TOKEN_WBTC;
+            tokenIn[5] = TOKEN_weETH;
+            tokenIn[6] = TOKEN_frxETH;
+            
+            tokenIn[7] = TOKEN_FRAX;
+            uint[] memory thresholdAmount = new uint[](8);
+            thresholdAmount[0] = 1e3;
+            thresholdAmount[1] = 1e3;
+            thresholdAmount[2] = 1e15;
+            thresholdAmount[3] = 1e3;
+            thresholdAmount[4] = 1e15;
+            thresholdAmount[5] = 1e15;
+            thresholdAmount[6] = 1e15;
+            thresholdAmount[7] = 1e15;
+            swaper.setThresholds(tokenIn, thresholdAmount);
+            LogDeployLib.logSetupSwapper(showLog);
+        }
+        //endregion -- Setup Swapper -----
+
+        //region ----- Add farms -----
+        factory.addFarms(farms());
+        LogDeployLib.logAddedFarms(address(factory), showLog);
+        //endregion -- Add farms -----
+
+        //region ----- Deploy strategy logics -----
+        _addStrategyLogic(factory, StrategyIdLib.COMPOUND_FARM, address(new CompoundFarmStrategy()), true);
+        LogDeployLib.logDeployStrategies(platform, showLog);
+
+        //endregion -- Deploy strategy logics -----
+
+        function _addVaultType(IFactory factory, string id, address implementation, uint buildingPrice) internal {
+            factory.setVaultConfig(
+                IFactory.VaultConfig({
+                    vaultType: id,
+                    implementation: implementation,
+                    deployAllowed: true,
+                    upgradeAllowed: true,
+                    buildingPrice: buildingPrice
+                })
+            )
+        }
+        function _addStrategyLogic(IFactory factory, string memory id, address implementation, bool farming) internal{
+             factory.setStrategyLogicConfig(
+                IFactory.StrategyLogicConfig({
+                    id: id,
+                    implementation: address(implementation),
+                    deployAllowed: true,
+                    upgradeAllowed: true,
+                    farming: farming,
+                    tokenId: type(uint).max
+                }),
+                StrategyDeveloperLib.getDeveloper(id)
+        );
+        }
     }
 }
