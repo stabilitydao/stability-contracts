7b37c9893f5216dc95e3050a31aa7f70a9f3c26a
interlinker0115 <interlinker422031@gmail.com>
2024-06-10 19:06:50 +1000

    fix: addvaulttype and addstrategylogic method

--- chains/ArbitrumLib.sol
+++ chains/ArbitrumLib.sol
@@ -23,64 +23,98 @@ import "../src/strategies/libs/StrategyIdLib.sol";
 /// @author Alien Deployer (https://github.com/a17)
 library ArbitrumLib {
     // initial addresses
-    address public constant MULTISIG = 0xE28e3Ee2bD10328bC8A7299B83A80d2E1ddD8708;
+    address public constant MULTISIG =
+        0xE28e3Ee2bD10328bC8A7299B83A80d2E1ddD8708;
 
     // ERC20
-    address public constant TOKEN_ARB = 0x912CE59144191C1204E64559FE8253a0e49E6548;
-    address public constant TOKEN_USDC = 0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
-    address public constant TOKEN_USDT = 0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9;
-    address public constant TOKEN_DAI = 0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
-    address public constant TOKEN_USDCe = 0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
-    address public constant TOKEN_WBTC = 0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
-    address public constant TOKEN_weETH = 0x35751007a407ca6FEFfE80b3cB397736D2cf4dbe;
-    address public constant TOKEN_frxETH = 0x178412e79c25968a32e89b11f63B33F733770c2A;
-    address public constant TOKEN_FRAX = 0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F;
-    
+    address public constant TOKEN_ARB =
+        0x912CE59144191C1204E64559FE8253a0e49E6548;
+    address public constant TOKEN_USDC =
+        0xaf88d065e77c8cC2239327C5EDb3A432268e5831;
+    address public constant TOKEN_USDT =
+        0xfd086bc7cd5c481dcc9c85ebe478a1c0b69fcbb9;
+    address public constant TOKEN_DAI =
+        0xDA10009cBd5D07dd0CeCc66161FC93D7c9000da1;
+    address public constant TOKEN_USDCe =
+        0xFF970A61A04b1cA14834A43f5dE4533eBDDB5CC8;
+    address public constant TOKEN_WBTC =
+        0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f;
+    address public constant TOKEN_weETH =
+        0x35751007a407ca6FEFfE80b3cB397736D2cf4dbe;
+    address public constant TOKEN_frxETH =
+        0x178412e79c25968a32e89b11f63B33F733770c2A;
+    address public constant TOKEN_FRAX =
+        0x17FC002b466eEc40DaE837Fc4bE5c67993ddBd6F;
+
     // AMMs
 
     // Oracles
-    address public constant ORACLE_CHAINLINK_USDC_USD = 0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3;
-    address public constant ORACLE_CHAINLINK_USDT_USD = 0x3f3f5dF88dC9F13eac63DF89EC16ef6e7E25DdE7;
-    address public constant ORACLE_CHAINLINK_DAI_USD = 0xc5C8E77B397E531B8EC06BFb0048328B30E9eCfB;
+    address public constant ORACLE_CHAINLINK_USDC_USD =
+        0x50834F3163758fcC1Df9973b6e91f0F0F0434aD3;
+    address public constant ORACLE_CHAINLINK_USDT_USD =
+        0x3f3f5dF88dC9F13eac63DF89EC16ef6e7E25DdE7;
+    address public constant ORACLE_CHAINLINK_DAI_USD =
+        0xc5C8E77B397E531B8EC06BFb0048328B30E9eCfB;
 
     // Compound
 
     // DeX aggregators
 
-    function platformDeployParams() internal pure returns(IPlatformDeployer.DeployPlatformParams memory p){
+    function platformDeployParams()
+        internal
+        pure
+        returns (IPlatformDeployer.DeployPlatformParams memory p)
+    {
         p.multisig = MULTISIG;
         p.version = "24.06.0-alpha";
         p.buildingPermitToken = address(0);
         p.buildingPayPerVaultToken = TOKEN_ARB;
         p.networkName = "Arbitrum";
-        p.networkExtra = CommonLib.bytesToBytes32(abi.encodePacked(bytes3(0x2959bc), bytes3(0x000000)));
+        p.networkExtra = CommonLib.bytesToBytes32(
+            abi.encodePacked(bytes3(0x2959bc), bytes3(0x000000))
+        );
         p.targetExchangeAsset = address(0);
         p.gelatoAutomate = GELATO_AUTOMATE;
         p.gelatoMinBalance = 1e16;
         p.gelatoDepositAmount = 2e16;
     }
 
-    function deployAndSetupInfrastructure(address platform, bool showLog) internal {
+    function deployAndSetupInfrastructure(
+        address platform,
+        bool showLog
+    ) internal {
         IFactory factory = IFactory(IPlatform(platform).factory());
-        
+
         //region ----- Deployed Platform -----
-        if(showLog) {
-            console.log("Deployed Statbility platform", IPlatform(platform).platformVersion());
+        if (showLog) {
+            console.log(
+                "Deployed Statbility platform",
+                IPlatform(platform).platformVersion()
+            );
             console.log("Platform addres:", platform);
         }
         //endregion -- Deployed Platform ----
 
         //region ----- Deploy and setup vault types -----
-        _addVaultType(factory, VaultTypeLib.COMPOUNDING, address(new CVault()), 100e6);
+        _addVaultType(
+            factory,
+            VaultTypeLib.COMPOUNDING,
+            address(new CVault()),
+            100e6
+        );
         //endregion -- Deploy and setup valut types -----
 
         //region -----Deploy and setup oracle adapters -----
-        IPriceReader priceReader = PriceReader(IPlatform(platform).priceReader());
+        IPriceReader priceReader = PriceReader(
+            IPlatform(platform).priceReader()
+        );
         //Chainlnk
         {
             Proxy proxy = new Proxy();
             proxy.initProxy(address(new ChainlinkAdapter()));
-            ChainlinkAdapter chainlinkAdapter = ChainlinkAdapter(address(proxy));
+            ChainlinkAdapter chainlinkAdapter = ChainlinkAdapter(
+                address(proxy)
+            );
             chainlinkAdapter.initialize(platform);
             address[] memory assets = new address[](3);
             assets[0] = TOKEN_USDC;
@@ -92,7 +126,11 @@ library ArbitrumLib {
             priceFeeds[2] = ORACLE_CHAINLINK_DAI_USD;
             chainlinkAdapter.addPriceFeeds(assets, priceFeeds);
             priceReader.addAdapter(address(chainlinkAdapter));
-            LogDeployLib.logDeployAndSetupOracleAdapter("ChainLink", address(chainlinkAdapter), showLog);
+            LogDeployLib.logDeployAndSetupOracleAdapter(
+                "ChainLink",
+                address(chainlinkAdapter),
+                showLog
+            );
         }
         //endregion -- Deploy and setup oracle adapters -----
 
@@ -104,7 +142,10 @@ library ArbitrumLib {
 
         //region ----- Setup Swapper -----
         {
-            (ISwapper.AddPoolData[] memory bcPools, ISwapper.AddPoolData[] memory pools) = routes();
+            (
+                ISwapper.AddPoolData[] memory bcPools,
+                ISwapper.AddPoolData[] memory pools
+            ) = routes();
             ISwapper swapper = ISwapper(IPlatform(platform).swapper());
             swapper.addBlueChipsPools(bcPools, false);
             swapper.addPools(pools, false);
@@ -116,7 +157,7 @@ library ArbitrumLib {
             tokenIn[4] = TOKEN_WBTC;
             tokenIn[5] = TOKEN_weETH;
             tokenIn[6] = TOKEN_frxETH;
-            
+
             tokenIn[7] = TOKEN_FRAX;
             uint[] memory thresholdAmount = new uint[](8);
             thresholdAmount[0] = 1e3;
@@ -138,34 +179,50 @@ library ArbitrumLib {
         //endregion -- Add farms -----
 
         //region ----- Deploy strategy logics -----
-        _addStrategyLogic(factory, StrategyIdLib.COMPOUND_FARM, address(new CompoundFarmStrategy()), true);
+        _addStrategyLogic(
+            factory,
+            StrategyIdLib.COMPOUND_FARM,
+            address(new CompoundFarmStrategy()),
+            true
+        );
         LogDeployLib.logDeployStrategies(platform, showLog);
 
         //endregion -- Deploy strategy logics -----
+    }
 
-        function _addVaultType(IFactory factory, string id, address implementation, uint buildingPrice) internal {
-            factory.setVaultConfig(
-                IFactory.VaultConfig({
-                    vaultType: id,
-                    implementation: implementation,
-                    deployAllowed: true,
-                    upgradeAllowed: true,
-                    buildingPrice: buildingPrice
-                })
-            )
-        }
-        function _addStrategyLogic(IFactory factory, string memory id, address implementation, bool farming) internal{
-             factory.setStrategyLogicConfig(
-                IFactory.StrategyLogicConfig({
-                    id: id,
-                    implementation: address(implementation),
-                    deployAllowed: true,
-                    upgradeAllowed: true,
-                    farming: farming,
-                    tokenId: type(uint).max
-                }),
-                StrategyDeveloperLib.getDeveloper(id)
+    function _addVaultType(
+        IFactory factory,
+        string id,
+        address implementation,
+        uint buildingPrice
+    ) internal {
+        factory.setVaultConfig(
+            IFactory.VaultConfig({
+                vaultType: id,
+                implementation: implementation,
+                deployAllowed: true,
+                upgradeAllowed: true,
+                buildingPrice: buildingPrice
+            })
         );
-        }
+    }
+
+    function _addStrategyLogic(
+        IFactory factory,
+        string memory id,
+        address implementation,
+        bool farming
+    ) internal {
+        factory.setStrategyLogicConfig(
+            IFactory.StrategyLogicConfig({
+                id: id,
+                implementation: address(implementation),
+                deployAllowed: true,
+                upgradeAllowed: true,
+                farming: farming,
+                tokenId: type(uint).max
+            }),
+            StrategyDeveloperLib.getDeveloper(id)
+        );
     }
 }
