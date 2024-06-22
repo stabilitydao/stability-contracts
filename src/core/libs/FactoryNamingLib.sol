af0cae93749fc0460a60dd8c917c5c5c703080a5
hcrypto7 <levilee9571@gmail.com>
2024-06-22 09:21:32 -0500

    fix: uneccessary import removed from factorynaminglib

--- src/core/libs/FactoryNamingLib.sol
+++ src/core/libs/FactoryNamingLib.sol
@@ -1,19 +1,12 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol";
 import "./CommonLib.sol";
 import "./VaultTypeLib.sol";
 import "../../interfaces/IPlatform.sol";
 import "../../interfaces/IStrategy.sol";
-import "../../interfaces/ISwapper.sol";
 import "../../interfaces/IFactory.sol";
-import "../../interfaces/IPriceReader.sol";
-import "../../interfaces/IVault.sol";
-import "../../interfaces/IRVault.sol";
-import "../../interfaces/IVaultProxy.sol";
-import "../../interfaces/IStrategyProxy.sol";
 
 library FactoryNamingLib {
     function getStrategyData(
