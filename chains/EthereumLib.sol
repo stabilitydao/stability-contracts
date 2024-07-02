9a19ad1beca06114583466c4d45ca8b7fee2c43c
interlinker0115 <interlinker422031@gmail.com>
2024-07-02 21:36:00 +0800

    init Ethereum lib draft

---
+++ chains/EthereumLib.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+/// @dev Ethereum network [chainId: 1] data library
+///   EEEEEEEEEE   TTTTTTTTTT  HHH    HHH   EEEEEEEEEE   RRRRRRRR    EEEEEEEEEE   UU     UU   M       M
+///   EEE              TTT     HHH    HHH   EEE          RR    RRR   EEE          UU     UU   MM     MM
+///   EEE              TTT     HHHHHHHHHH   EEE          RRRRRRR     EEE          UU     UU   MM M M MM
+///   EEEEEEEE         TTT     HHHHHHHHHH   EEEEEEEE     RRR   RR    EEEEEEEE     UU     UU   MM  M  MM
+///   EEE              TTT     HHH    HHH   EEE          RR    RR    EEE          UU     UU   MM     MM
+///   EEE              TTT     HHH    HHH   EEE          RR     RR   EEE          UU     UU   MM     MM
+///   EEEEEEEEEE       TTT     HHH    HHH   EEEEEEEEEE   RR      RR  EEEEEEEEEE    UUUUUUU    MM     MM
+/// @author Interlinker (https://github.com/Interlinker0115)
+library EthereumLib {
+
+}
