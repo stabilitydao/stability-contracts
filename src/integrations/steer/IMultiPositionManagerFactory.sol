976aee1cdba082a466472d50850ec78c26af116a
hcrypto7 <levilee9571@gmail.com>
2024-07-19 11:03:02 -0500

    fix: forge fmt

--- src/integrations/steer/IMultiPositionManagerFactory.sol
+++ src/integrations/steer/IMultiPositionManagerFactory.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8.23;
 
 interface IMultiPositionManagerFactory {
-
     function getHeartBeat(address _base, address _quote) external view returns (uint);
 
     function chainlinkRegistry() external view returns (address);
