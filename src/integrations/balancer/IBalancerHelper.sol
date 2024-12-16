00aa2afe4ca6ce6d7c121039bf79205b5f8fa40c
Alien Deployer <m@a17.name>
2024-12-17 01:25:53 +0300

    lint

--- src/integrations/balancer/IBalancerHelper.sol
+++ src/integrations/balancer/IBalancerHelper.sol
@@ -2,28 +2,28 @@
 pragma solidity ^0.8.23;
 
 interface IBalancerHelper {
-  function queryExit(
-    bytes32 poolId,
-    address sender,
-    address recipient,
-    IVault.JoinPoolRequest memory request
-  ) external returns (uint256 bptIn, uint256[] memory amountsOut);
+    function queryExit(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        IVault.JoinPoolRequest memory request
+    ) external returns (uint bptIn, uint[] memory amountsOut);
 
-  function queryJoin(
-    bytes32 poolId,
-    address sender,
-    address recipient,
-    IVault.JoinPoolRequest memory request
-  ) external returns (uint256 bptOut, uint256[] memory amountsIn);
+    function queryJoin(
+        bytes32 poolId,
+        address sender,
+        address recipient,
+        IVault.JoinPoolRequest memory request
+    ) external returns (uint bptOut, uint[] memory amountsIn);
 
-  function vault() external view returns (address);
+    function vault() external view returns (address);
 }
 
 interface IVault {
-  struct JoinPoolRequest {
-    address[] assets;
-    uint256[] maxAmountsIn;
-    bytes userData;
-    bool fromInternalBalance;
-  }
+    struct JoinPoolRequest {
+        address[] assets;
+        uint[] maxAmountsIn;
+        bytes userData;
+        bool fromInternalBalance;
+    }
 }
