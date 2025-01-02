33e0ae59787bb43ae3b32a1e187da31e7d9849ad
Alien Deployer <m@a17.name>
2025-01-02 03:03:56 +0300

    fix solidly adapter deploy script

--- script/deploy-adapter/Solidly.Sonic.s.sol
+++ script/deploy-adapter/Solidly.Sonic.s.sol
@@ -5,7 +5,7 @@ import "forge-std/Script.sol";
 import "../../src/core/proxy/Proxy.sol";
 import "../../src/adapters/SolidlyAdapter.sol";
 
-contract DeploySolidlyAdapterReal is Script {
+contract DeploySolidlyAdapterSonic is Script {
     address public constant PLATFORM = 0x4Aca671A420eEB58ecafE83700686a2AD06b20D8;
 
     function run() external {
