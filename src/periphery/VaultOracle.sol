13faa7ef418ef6bf2381bcdbc5326154f1e6ab96
Alien Deployer <m@a17.name>
2025-01-20 23:49:04 +0300

    mitigate slither alerts

--- src/periphery/VaultOracle.sol
+++ src/periphery/VaultOracle.sol
@@ -10,13 +10,16 @@ contract VaultOracle is IAggregatorInterfaceMinimal {
     address public immutable vault;
 
     constructor(address vault_) {
+        // slither-disable-next-line missing-zero-check
         vault = vault_;
+        // slither-disable-next-line unused-return
         (, bool trusted) = IVault(vault_).price();
         require(trusted, "Not trusted");
     }
 
     /// @inheritdoc IAggregatorInterfaceMinimal
     function latestAnswer() external view returns (int) {
+        // slither-disable-next-line unused-return
         (uint price,) = IVault(vault).price();
         return int(price / 10 ** 10);
     }
