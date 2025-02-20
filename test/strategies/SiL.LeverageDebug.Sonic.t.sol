2a4c773190f2555f26b8b20c1c361f95f80ebe94
Alien Deployer <m@a17.name>
2025-02-20 13:32:05 +0300

    SiL debug test

---
+++ test/strategies/SiL.LeverageDebug.Sonic.t.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SonicLib} from "../../chains/SonicLib.sol";
+import {ILeverageLendingStrategy} from "../../src/interfaces/ILeverageLendingStrategy.sol";
+import {IPlatform} from "../../src/interfaces/IPlatform.sol";
+import {IControllable} from "../../src/interfaces/IControllable.sol";
+import {IStrategy} from "../../src/interfaces/IStrategy.sol";
+import {IFactory} from "../../src/interfaces/IFactory.sol";
+import {IVault} from "../../src/interfaces/IVault.sol";
+import {Vm, console, Test} from "forge-std/Test.sol";
+import {SiloLeverageStrategy} from "../../src/strategies/SiloLeverageStrategy.sol";
+import {StrategyIdLib} from "../../src/strategies/libs/StrategyIdLib.sol";
+import {CommonLib} from "../../src/core/libs/CommonLib.sol";
+
+contract SiloLeverageLendingStrategyDebugTest is Test {
+    address public constant PLATFORM = 0x4Aca671A420eEB58ecafE83700686a2AD06b20D8;
+    //address public constant STRATEGY = 0x811002015AC45D551A3D962d8375A7B16Dede6BE; // S-stS
+    address public constant STRATEGY = 0xfF9C35acDA4b136F71B1736B2BDFB5479f111C4A; // stS-S
+    address public vault;
+    address public multisig;
+    IFactory public factory;
+
+    constructor() {
+        vm.selectFork(vm.createFork(vm.envString("SONIC_RPC_URL")));
+        //vm.rollFork(8340694);
+        vault = IStrategy(STRATEGY).vault();
+        multisig = IPlatform(IControllable(STRATEGY).platform()).multisig();
+        factory = IFactory(IPlatform(IControllable(STRATEGY).platform()).factory());
+    }
+
+    function testSiLHardWork() public {
+        //deal(SonicLib.TOKEN_wS, STRATEGY, 500e18);
+        //_deposit(10000e18);
+        //vm.roll(block.number + 6);
+        //_withdrawAll();
+
+        vm.recordLogs();
+        vm.prank(vault);
+        IStrategy(STRATEGY).doHardWork();
+        Vm.Log[] memory entries = vm.getRecordedLogs();
+        for (uint j = 0; j < entries.length; ++j) {
+            if (
+                entries[j].topics[0]
+                    == keccak256("LeverageLendingHardWork(int256,int256,uint256,uint256,uint256,uint256,uint256)")
+            ) {
+                (int realApr,,,,,,) = abi.decode(entries[j].data, (int, int, uint, uint, uint, uint, uint));
+                console.log(string.concat("    Real APR: ", CommonLib.formatAprInt(realApr), "."));
+            }
+        }
+    }
+
+    /*function testSiLRebalanceDebt() public {
+        ILeverageLendingStrategy s = ILeverageLendingStrategy(STRATEGY);
+        (uint ltv,,,,,) = s.health();
+        console.log('LTV', ltv);
+
+        vm.startPrank(multisig);
+
+        uint newLtv = 9490;
+        console.log('New LTV', newLtv);
+        uint resultLtv = s.rebalanceDebt(newLtv);
+        console.log('Result LTV', resultLtv);
+    }*/
+
+    function _deposit(uint depositAmount) internal {
+        address[] memory assets = IStrategy(STRATEGY).assets();
+        deal(assets[0], address(this), depositAmount);
+        IERC20(assets[0]).approve(vault, depositAmount);
+        uint[] memory amounts = new uint[](1);
+        amounts[0] = depositAmount;
+        IVault(vault).depositAssets(assets, amounts, 0, address(this));
+    }
+
+    function _withdrawAll() internal {
+        address[] memory assets = IStrategy(STRATEGY).assets();
+        uint bal = IERC20(vault).balanceOf(address(this));
+        IVault(vault).withdrawAssets(assets, bal, new uint[](1));
+    }
+
+    function _upgrade() internal {
+        address strategyImplementation = address(new SiloLeverageStrategy());
+        factory.setStrategyLogicConfig(
+            IFactory.StrategyLogicConfig({
+                id: StrategyIdLib.SILO_LEVERAGE,
+                implementation: strategyImplementation,
+                deployAllowed: true,
+                upgradeAllowed: true,
+                farming: false,
+                tokenId: 0
+            }),
+            address(this)
+        );
+        factory.upgradeStrategyProxy(STRATEGY);
+    }
+}
