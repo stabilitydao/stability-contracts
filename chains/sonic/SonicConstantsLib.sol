dcd129c38bb12b544f6f2917e70e1d3ac93d71c5
Alien Deployer <m@a17.name>
2025-05-06 02:35:19 +0300

    add silo vault 49 usdc

--- chains/sonic/SonicConstantsLib.sol
+++ chains/sonic/SonicConstantsLib.sol
@@ -299,6 +299,7 @@ library SonicConstantsLib {
     address public constant SILO_VAULT_27_USDC = 0x7e88AE5E50474A48deA4c42a634aA7485e7CaA62;
     address public constant SILO_VAULT_51_wS = 0xC92E96acb616476Ed117f263Fa68Ce21F6D554D3;
     address public constant SILO_VAULT_31_WBTC = 0x0588651eE0B84b3CA8035a69d60fF18c0263DF71;
+    address public constant SILO_VAULT_49_USDC = 0xa18a8f100f2c976044f2f84fae1eE9f807Ae7893;
 
     // Gamma
     address public constant GAMMA_UNISWAPV3_UNIPROXY = 0xcD5A60eb030300661cAf97244aE98e1D5A70f2c8;
