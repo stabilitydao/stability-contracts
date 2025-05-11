923c3cca4e2a9ce618903e838b855c18d3e382c2
Alien Deployer <m@a17.name>
2025-05-11 22:18:27 +0300

    MetaVaultProxy

---
+++ src/core/proxy/MetaVaultProxy.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {UpgradeableProxy} from "../../core/base/UpgradeableProxy.sol";
+import {IControllable} from "../../interfaces/IControllable.sol";
+import {IPlatform} from "../../interfaces/IPlatform.sol";
+import {IMetaVaultFactory} from "../../interfaces/IMetaVaultFactory.sol";
+import {IMetaProxy} from "../../interfaces/IMetaProxy.sol";
+
+/// @title EIP1967 Upgradeable proxy implementation for MetaVaults
+///         ┏┓┏┳┓┏┓┳┓┳┓ ┳┏┳┓┓┏  ┏┓┓ ┏┓┏┳┓┏┓┏┓┳┓┳┳┓
+///         ┗┓ ┃ ┣┫┣┫┃┃ ┃ ┃ ┗┫  ┃┃┃ ┣┫ ┃ ┣ ┃┃┣┫┃┃┃
+///         ┗┛ ┻ ┛┗┻┛┻┗┛┻ ┻ ┗┛  ┣┛┗┛┛┗ ┻ ┻ ┗┛┛┗┛ ┗
+/// @author Alien Deployer (https://github.com/a17)
+contract MetaVaultProxy is UpgradeableProxy, IMetaProxy {
+    /// @inheritdoc IMetaProxy
+    function initProxy() external {
+        _init(IMetaVaultFactory(msg.sender).metaVaultImplementation());
+    }
+
+    /// @inheritdoc IMetaProxy
+    function upgrade() external {
+        require(msg.sender == IPlatform(IControllable(address(this)).platform()).metaVaultFactory(), ProxyForbidden());
+        _upgradeTo(IMetaVaultFactory(msg.sender).metaVaultImplementation());
+    }
+
+    /// @inheritdoc IMetaProxy
+    function implementation() external view returns (address) {
+        return _implementation();
+    }
+}
