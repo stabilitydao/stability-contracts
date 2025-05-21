9bf6ee7fdeef27ad89d2a770f306d0ddf4b9c1b2
Alien Deployer <m@a17.name>
2025-05-21 22:35:18 +0300

    rename test and deploy script

--- script/deploy-strategy/E.s.sol
+++ script/deploy-strategy/E.s.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.28;
 import {Script} from "forge-std/Script.sol";
 import {EulerStrategy} from "../../src/strategies/EulerStrategy.sol";
 
-contract DeployStrategyM is Script {
+contract DeployE is Script {
     function run() external {
         uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
