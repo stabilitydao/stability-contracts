803d1af88f883a13d305f3f7e008deb813e44a0e
Alien Deployer <m@a17.name>
2025-06-03 21:57:11 +0300

    #296 fix pragma

--- src/periphery/VaultOracle.sol
+++ src/periphery/VaultOracle.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.23;
+pragma solidity ^0.8.28;
 
 import {IAggregatorInterfaceMinimal} from "../integrations/chainlink/IAggregatorInterfaceMinimal.sol";
 import {IVault} from "../interfaces/IVault.sol";
