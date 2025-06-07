e800cdcee52ebdb662e88e2104e62ff5e6ae7d00
Alien Deployer <m@a17.name>
2025-06-07 21:06:54 +0300

    🧑‍🔧 add all vaults used in meta vaults

--- chains/sonic/SonicConstantsLib.sol
+++ chains/sonic/SonicConstantsLib.sol
@@ -15,26 +15,33 @@ library SonicConstantsLib {
     address public constant WRAPPED_METAVAULT_metaUSDC = 0xEEEEEEE6d95E55A468D32FeB5d6648754d10A967;
     address public constant METAVAULT_metascUSD = 0x33333333C480194b5B651987b7D00B20dDCbd287;
     address public constant WRAPPED_METAVAULT_metascUSD = 0xccccCCcca9FC69a2b32408730011EdB3205A93A1;
-    address public constant METAVAULT_metawS = 0x555555554776B14B30597d1032E48f9e16db22A4;
-    address public constant WRAPPED_METAVAULT_metawS = 0xffFFFFFf2fcBeFAe12F1372C56edC769BD411685;
     address public constant METAVAULT_metaS = 0x4444444420D9De54d69b3997b7D6A31d2BF63F32;
     address public constant WRAPPED_METAVAULT_metaS = 0xbbbbbbBBbd0aE69510cE374A86749f8276647B19;
+    address public constant METAVAULT_metawS = 0x555555554776B14B30597d1032E48f9e16db22A4;
+    address public constant WRAPPED_METAVAULT_metawS = 0xffFFFFFf2fcBeFAe12F1372C56edC769BD411685;
 
-    // Vaults
+    // Vaults used in MetaVaults
+    // metaUSDC
     address public constant VAULT_C_USDC_SiF = 0xa51e7204054464e656B3658e7dBb63d9b0f150f1;
     address public constant VAULT_C_USDC_S_8 = 0x96a8055090E87bfE18BdF3794E9D676F196EFd80;
     address public constant VAULT_C_USDC_S_27 = 0x2ebB3c7808B86f94dF9731AE830aB6ea8cB431d8;
     address public constant VAULT_C_USDC_S_34 = 0xd248c4b6Ec709FEeD32851A9F883AfeaC294aD30;
     address public constant VAULT_C_USDC_S_36 = 0x38274302e0Dd5779b4E0A3E401023cFB48fF5c23;
     address public constant VAULT_C_USDC_S_49 = 0xc33568559c8338581BB6914d6F2d024a063886E8;
-    address public constant VAULT_C_USDC_scUSD_ISF_scUSD = 0x8C64D2a1960C7B4b22Dbb367D2D212A21E75b942;
-    address public constant VAULT_C_USDC_scUSD_ISF_USDC = 0xb773B791F3baDB3b28BC7A2da18E2a012b9116c2;
-    address public constant VAULT_C_scUSD_S_46 = 0x14d17757e88Df8f59069fFa573570A50ed652866;
-    address public constant VAULT_C_USDC_Stability_Main = 0x402ae122CaEce6ce57203e3Bd4AF7d1e9Ac446cb;
     address public constant VAULT_C_USDC_Stability_Stream = 0x0c8cE5afC38C94e163F0dDEB2Da65DF4904734f3;
     address public constant VAULT_C_USDC_Stability_StableJack = 0x7FC269E8A80d4cFbBCfaB99A6BcEAC06227E2336;
+    // metascUSD
+    address public constant VAULT_C_scUSD_S_46 = 0x14d17757e88Df8f59069fFa573570A50ed652866;
     address public constant VAULT_C_scUSD_Euler_MevCapital = 0xfbb02D242527f3B56AF3f087AcddaA25D5E8ACC4;
     address public constant VAULT_C_scUSD_Euler_Re7Labs = 0xB94b31f3dEdaADbdA6AA1cEc4f49749273929972;
+    // metawS
+    address public constant VAULT_C_wS_SiMF_Valmore = 0xA70F074EE09DF3C0AB7e9eb0c058941fF65B8Efe;
+    address public constant VAULT_C_USDC_SiF_54 = 0x86a4a379d9a2a3B3f0e1f8Aa477c82f646344458;
+
+    // used in tests
+    address public constant VAULT_C_USDC_scUSD_ISF_scUSD = 0x8C64D2a1960C7B4b22Dbb367D2D212A21E75b942;
+    address public constant VAULT_C_USDC_scUSD_ISF_USDC = 0xb773B791F3baDB3b28BC7A2da18E2a012b9116c2;
+    //address public constant VAULT_C_USDC_Stability_Main = 0x402ae122CaEce6ce57203e3Bd4AF7d1e9Ac446cb;
 
     // ERC20
     address public constant TOKEN_wS = 0x039e2fB66102314Ce7b64Ce5Ce3E5183bc94aD38;
