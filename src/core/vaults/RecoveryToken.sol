1e1bead59609472037aba186c6a462f28df1b15e
Alien Deployer <m@a17.name>
2025-08-12 13:51:01 +0300

    🧑‍🔧 RecoveryToken: cleanup

--- src/core/vaults/RecoveryToken.sol
+++ src/core/vaults/RecoveryToken.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {console} from "forge-std/console.sol";
-
 import {Controllable, IControllable, IPlatform} from "../base/Controllable.sol";
 import {
     ERC20Upgradeable,
