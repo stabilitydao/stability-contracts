3e1990cd387bf5f42e7165d9c9ebc82c389079bd
dvpublic <dvpublic0@gmail.com>
2025-09-30 14:06:26 +0700

    remove unused functions

--- test/tokenomics/Recovery.Sonic.t.sol
+++ test/tokenomics/Recovery.Sonic.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.28;
 
-import {console} from "forge-std/console.sol";
+// import {console} from "forge-std/console.sol";
 import {SonicLib} from "../../chains/sonic/SonicLib.sol";
 import {AmmAdapterIdLib} from "../../src/adapters/libs/AmmAdapterIdLib.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
