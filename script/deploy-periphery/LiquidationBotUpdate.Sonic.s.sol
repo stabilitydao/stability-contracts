2ffcf6578cffd19e5119537401e9a096609d859f
dvpublic <dvpublic0@gmail.com>
2025-10-13 21:38:50 +0700

    fix liq bot deploy scripts

--- script/deploy-periphery/LiquidationBotUpdate.Sonic.s.sol
+++ script/deploy-periphery/LiquidationBotUpdate.Sonic.s.sol
@@ -5,15 +5,13 @@ import {Script} from "forge-std/Script.sol";
 import {Proxy} from "../../src/core/proxy/Proxy.sol";
 import {LiquidationBot} from "../../src/periphery/LiquidationBot.sol";
 
-contract DeployLiquidationBot is Script {
+contract DeployLiquidationBotUpdate is Script {
     address public constant PLATFORM = 0x4Aca671A420eEB58ecafE83700686a2AD06b20D8;
 
     function run() external {
         uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         vm.startBroadcast(deployerPrivateKey);
-        Proxy proxy = new Proxy();
-        proxy.initProxy(address(new LiquidationBot()));
-        LiquidationBot(address(proxy)).initialize(PLATFORM);
+        new LiquidationBot();
         vm.stopBroadcast();
     }
 
