c9b26c645f483218d3c6038ba9fc92bf4866d707
omriss <omriss.omega@gmail.com>
2025-10-29 17:04:28 +0700

    add dependencies, draft test to send token through stbl bridge

--- test/tokenomics/BridgedSTBL.t.sol
+++ test/tokenomics/BridgedSTBL.t.sol
@@ -1,48 +1,154 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import {console} from "forge-std/console.sol";
 import {BridgedSTBL} from "../../src/tokenomics/BridgedSTBL.sol";
 import {STBLOFTAdapter} from "../../src/tokenomics/STBLOFTAdapter.sol";
 import {IPlatform} from "../../src/interfaces/IPlatform.sol";
 import {SonicConstantsLib} from "../../chains/sonic/SonicConstantsLib.sol";
 import {Test} from "forge-std/Test.sol";
 import {Proxy} from "../../src/core/proxy/Proxy.sol";
-import {console} from "forge-std/console.sol";
 import {AvalancheConstantsLib} from "../../chains/avalanche/AvalancheConstantsLib.sol";
+import {SendParam, IOFT} from "@layerzerolabs/oft-evm/contracts/interfaces/IOFT.sol";
+import {MessagingFee} from "@layerzerolabs/oapp-evm/contracts/oapp/OAppSender.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {Origin} from "@layerzerolabs/oapp-evm/contracts/oapp/interfaces/IOAppReceiver.sol";
+import {OFTMsgCodec} from "@layerzerolabs/oft-evm/contracts/libs/OFTMsgCodec.sol";
+import {OptionsBuilder} from "@layerzerolabs/oapp-evm/contracts/oapp/libs/OptionsBuilder.sol";
 
 contract BridgedSTBLTest is Test {
-    address public multisig;
+    using OptionsBuilder for bytes;
+
+    address public multisigSonic;
+    address public multisigAvalanche;
 
     uint private constant SONIC_FORK_BLOCK = 52228979; // Oct-28-2025 01:14:21 PM +UTC
     uint private constant AVALANCHE_FORK_BLOCK = 71037861; // Oct-28-2025 13:17:17 UTC
 
-    /// @dev https://docs.layerzero.network/v2/deployments/deployed-contracts
-    address private constant ENDPOINT_V2_AVALANCHE = 0x1a44076050125825900e736c501f859c50fE728c; // todo to constants lib
-    address private constant ENDPOINT_V2_SONIC = 0x6F475642a6e85809B1c36Fa62763669b1b48DD5B;
-
     uint internal forkSonic;
     uint internal forkAvalanche;
+
+    STBLOFTAdapter internal adapter;
+    BridgedSTBL internal bridgedToken;
 
     constructor() {
         forkSonic = vm.createFork(vm.envString("SONIC_RPC_URL"), SONIC_FORK_BLOCK);
         forkAvalanche = vm.createFork(vm.envString("AVALANCHE_RPC_URL"), AVALANCHE_FORK_BLOCK);
 
-        vm.selectFork(forkSonic);
-        multisig = IPlatform(SonicConstantsLib.PLATFORM).multisig();
-    }
+        // ------------------- Create adapter and bridged token
+        bridgedToken = BridgedSTBL(setupSTBLBridgedOnAvalanche());
+        adapter = STBLOFTAdapter(setupSTBLOFTAdapterOnSonic());
 
-    function testInit() public pure {
-        console.logBytes32(
-            keccak256(abi.encode(uint(keccak256("erc7201:stability.BridgedSTBL")) - 1)) & ~bytes32(uint(0xff))
+        // ------------------- Sonic: set up peer connection
+        vm.selectFork(forkSonic);
+        multisigSonic = IPlatform(SonicConstantsLib.PLATFORM).multisig();
+
+        vm.prank(multisigSonic);
+        adapter.setPeer(
+            AvalancheConstantsLib.LAYER_ZERO_V2_ENDPOINT_ID,
+            bytes32(uint256(uint160(address(bridgedToken))))
+        );
+
+        // ------------------- Avalanche: set up peer connection
+        vm.selectFork(forkAvalanche);
+        multisigAvalanche = IPlatform(AvalancheConstantsLib.PLATFORM).multisig();
+
+        vm.prank(multisigAvalanche);
+        bridgedToken.setPeer(
+            SonicConstantsLib.LAYER_ZERO_V2_ENDPOINT_ID,
+            bytes32(uint256(uint160(address(adapter))))
         );
     }
 
-    function testBridge() public {
-        address stblAvalanche = setupSTBLBridgedOnAvalanche();
-        address adapter = setupSTBLOFTAdapterOnSonic();
+    function testViewSTBLOFTAdapter() public {
+        vm.selectFork(forkSonic);
 
-        // todo
-        // STBLBridged(adapter).send();
+        assertEq(adapter.owner(), multisigSonic);
+    }
+
+    function testViewBridgedStbl() public {
+        vm.selectFork(forkAvalanche);
+
+        console.logBytes32(
+            keccak256(abi.encode(uint(keccak256("erc7201:stability.BridgedSTBL")) - 1)) & ~bytes32(uint(0xff))
+        );
+
+        assertEq(bridgedToken.name(), "Stability STBL");
+        assertEq(bridgedToken.symbol(), "STBLb");
+        assertEq(bridgedToken.owner(), multisigAvalanche);
+        assertEq(bridgedToken.decimals(), 18);
+    }
+
+    function testSendToAvalanche() public {
+        // ------------------ Sonic: user sends tokens to himself on Avalanche
+        vm.selectFork(forkSonic);
+
+        address sender = address(this);
+        uint sendAmount = 500e18;
+        uint balance0 = 800e18;
+
+        deal(SonicConstantsLib.TOKEN_STBL, address(this), balance0);
+
+        IERC20(SonicConstantsLib.TOKEN_STBL).approve(address(adapter), sendAmount);
+
+        bytes memory options = OptionsBuilder.newOptions().addExecutorLzReceiveOption(2_000_000, 0);
+
+        SendParam memory sendParam = SendParam({
+            dstEid: AvalancheConstantsLib.LAYER_ZERO_V2_ENDPOINT_ID,
+            to: bytes32(uint256(uint160(address(this)))),
+            amountLD: sendAmount,
+            minAmountLD: sendAmount,
+            extraOptions: options,
+            composeMsg: "",
+            oftCmd: ""
+        });
+
+        assertEq(IERC20(SonicConstantsLib.TOKEN_STBL).balanceOf(sender), balance0, "balance STBL before");
+        assertEq(IERC20(SonicConstantsLib.TOKEN_STBL).balanceOf(address(adapter)), 0, "no tokens in adapter");
+
+        // ------------------- Prepare fee
+        MessagingFee memory msgFee = adapter.quoteSend(sendParam, false);
+        deal(sender, 1 ether);
+        console.log("1");
+
+        // ------------------- Send
+        vm.prank(sender);
+        adapter.send{value: msgFee.nativeFee}(sendParam, msgFee, sender);
+        console.log("1");
+
+        // ------------------- Check results
+        assertEq(IERC20(SonicConstantsLib.TOKEN_STBL).balanceOf(sender), balance0 - sendAmount, "balance STBL after");
+        assertEq(IERC20(SonicConstantsLib.TOKEN_STBL).balanceOf(address(adapter)), sendAmount, "all tokens are in adapter");
+
+        // ------------------ Avalanche: simulate message reception
+        vm.selectFork(forkAvalanche);
+
+        console.log("1");
+        (bytes memory oftMessage,) = OFTMsgCodec.encode(
+            bytes32(uint256(uint160(sender))), // to
+            uint64(sendAmount),                        // amountLD
+            ""                                 // composeMsg
+        );
+
+        console.log("1");
+        Origin memory origin = Origin({
+            srcEid: SonicConstantsLib.LAYER_ZERO_V2_ENDPOINT_ID,
+            sender: bytes32(uint256(uint160(address(adapter)))),
+            nonce: 1
+        });
+
+        console.log("1");
+        vm.prank(AvalancheConstantsLib.LAYER_ZERO_V2_ENDPOINT);
+        bridgedToken.lzReceive(
+            origin,
+            bytes32(0), // guid
+            oftMessage,
+            address(0), // executor
+            ""          // extraData
+        );
+        console.log("1");
+
+        assertEq(bridgedToken.balanceOf(address(this)), sendAmount, "user received tokens on Avalanche");
     }
 
     //region ------------------------------------- Internal logic
@@ -50,7 +156,7 @@ contract BridgedSTBLTest is Test {
         vm.selectFork(forkAvalanche);
 
         Proxy proxy = new Proxy();
-        proxy.initProxy(address(new BridgedSTBL(ENDPOINT_V2_AVALANCHE)));
+        proxy.initProxy(address(new BridgedSTBL(AvalancheConstantsLib.LAYER_ZERO_V2_ENDPOINT)));
         BridgedSTBL stblBridged = BridgedSTBL(address(proxy));
         stblBridged.initialize(address(AvalancheConstantsLib.PLATFORM));
 
@@ -61,7 +167,7 @@ contract BridgedSTBLTest is Test {
         vm.selectFork(forkSonic);
 
         Proxy proxy = new Proxy();
-        proxy.initProxy(address(new STBLOFTAdapter(SonicConstantsLib.TOKEN_STBL, ENDPOINT_V2_SONIC)));
+        proxy.initProxy(address(new STBLOFTAdapter(SonicConstantsLib.TOKEN_STBL, SonicConstantsLib.LAYER_ZERO_V2_ENDPOINT)));
         STBLOFTAdapter stblOFTAdapter = STBLOFTAdapter(address(proxy));
         stblOFTAdapter.initialize(address(SonicConstantsLib.PLATFORM));
 
