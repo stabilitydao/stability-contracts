a9138ebf94ecfb6fa9e4b2ddcd61549354344ce1
omriss <omriss.omega@gmail.com>
2025-10-30 19:11:52 +0700

    platform upgrade 25.10.5-alpha

---
+++ script/upgrade-core/PrepareUpgrade.25.10.5-alpha.s.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {SonicConstantsLib} from "../../chains/sonic/SonicConstantsLib.sol";
+import {Platform} from "../../src/core/Platform.sol";
+import {Script} from "forge-std/Script.sol";
+import {StabilityDAO} from "../../src/tokenomics/StabilityDAO.sol";
+
+contract PrepareUpgrade25105alpha is Script {
+    address public constant PLATFORM = SonicConstantsLib.PLATFORM;
+
+    function run() external {
+        uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        // StabilityDAO 1.0.1
+        new StabilityDAO();
+
+        vm.stopBroadcast();
+    }
+
+    function testPrepareUpgrade() external {}
+}
