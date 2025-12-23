41d8172ab2e352665cc78ab5bae8f0c508aa3713
omriss <omriss.omega@gmail.com>
2025-12-23 17:56:04 +0700

    deploy SiAL for USDp on Avalanche

--- script/deploy-adapter/BalancerV3Stable.s.sol
+++ script/deploy-adapter/BalancerV3Stable.s.sol
@@ -1,20 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import {StdConfig} from "forge-std/StdConfig.sol";
+import {Variable, LibVariable} from "forge-std/LibVariable.sol";
 import {Script} from "forge-std/Script.sol";
 import {Proxy} from "../../src/core/proxy/Proxy.sol";
 import {BalancerV3StableAdapter} from "../../src/adapters/BalancerV3StableAdapter.sol";
-import {PlasmaConstantsLib} from "../../chains/plasma/PlasmaConstantsLib.sol";
 
 contract DeployBalancerV3StableAdapterPlasma is Script {
-    address public constant PLATFORM = PlasmaConstantsLib.PLATFORM;
+    using LibVariable for Variable;
 
     function run() external {
         uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        StdConfig config = new StdConfig("./config.toml", false); // read only config
+        address platform = config.get("PLATFORM").toAddress();
+
         vm.startBroadcast(deployerPrivateKey);
         Proxy proxy = new Proxy();
         proxy.initProxy(address(new BalancerV3StableAdapter()));
-        BalancerV3StableAdapter(address(proxy)).init(PLATFORM);
+        BalancerV3StableAdapter(address(proxy)).init(platform);
         vm.stopBroadcast();
     }
 
--- script/deploy-adapter/ERC4626.s.sol
+++ script/deploy-adapter/ERC4626.s.sol
@@ -1,19 +1,26 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.23;
 
+import {StdConfig} from "forge-std/StdConfig.sol";
+import {Variable, LibVariable} from "forge-std/LibVariable.sol";
 import {Script} from "forge-std/Script.sol";
 import {Proxy} from "../../src/core/proxy/Proxy.sol";
 import {ERC4626Adapter} from "../../src/adapters/ERC4626Adapter.sol";
+import {PlasmaConstantsLib} from "../../chains/plasma/PlasmaConstantsLib.sol";
 
-contract DeployERC4626AdapterSonic is Script {
-    address public constant PLATFORM = 0x4Aca671A420eEB58ecafE83700686a2AD06b20D8;
+contract DeployERC4626AdapterPlasma is Script {
+    using LibVariable for Variable;
 
     function run() external {
         uint deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+
+        StdConfig config = new StdConfig("./config.toml", false); // read only config
+        address platform = config.get("PLATFORM").toAddress();
+
         vm.startBroadcast(deployerPrivateKey);
         Proxy proxy = new Proxy();
         proxy.initProxy(address(new ERC4626Adapter()));
-        ERC4626Adapter(address(proxy)).init(PLATFORM);
+        ERC4626Adapter(address(proxy)).init(platform);
         vm.stopBroadcast();
     }
 
